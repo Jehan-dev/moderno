e3d635e1698537af40793ec22710c07e2ec99387
Jehan-dev <evgenbuz@inbox.ru>
2019-09-18 15:15:28 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function() {
 
+    var mixer = mixitup('.products__inner-box');
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+    });
+
+    $('.product__slider__inner').slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false
+    })
 
 
 });
