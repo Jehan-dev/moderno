0e667114d6daa76be4b4357e44d9ab993e00a804
Jehan-dev <evgenbuz@inbox.ru>
2019-09-25 17:46:06 +0300

    added new-items-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 $(function() {
 
-    var mixer = mixitup('.products__inner-box');
-
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
@@ -13,7 +11,29 @@ $(function() {
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: false
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+         type: "double",
+             min: 0,
+             max: 1000,
+             from: 0,
+             to: 600,
+             prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function () {
+        $('.product-page__items .product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    })
+    $('.icon-th-large').on('click', function () {
+        $('.product-page__items .product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
     })
 
+
+    var mixer = mixitup('.products__inner-box');
 
 });
