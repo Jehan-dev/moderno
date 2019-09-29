fc419c9ba7a17d9e6e0163bdd2f2595d20651dd5
Jehan-dev <evgenbuz@inbox.ru>
2019-09-29 13:13:01 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -31,7 +31,15 @@ $(function() {
         $('.product-page__items .product__item').removeClass('list');
         $('.icon-th-large').addClass('active');
         $('.icon-th-list').removeClass('active');
-    })
+    });
+
+    $('.header__btn-menu').on('click', function () {
+        $('.header__box').toggleClass('active');       
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();       
+    });
 
 
     var mixer = mixitup('.products__inner-box');
