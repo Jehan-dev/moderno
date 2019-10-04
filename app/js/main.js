bbc055fe5d4793426e33f91aad77bc2f19740c2e
Jehan-dev <evgenbuz@inbox.ru>
2019-10-04 17:21:53 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,31 @@ $(function() {
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 4,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ],
+
     });
 
     $(".js-range-slider").ionRangeSlider({
